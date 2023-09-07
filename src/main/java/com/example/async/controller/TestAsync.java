9a55348538b7df95b0bc2174fa34051d0ce03058
lichengcan <li929825769@163.com>
2023-09-07 16:27:37 +0800

    异步初始化

---
+++ src/main/java/com/example/async/controller/TestAsync.java
@@ -0,0 +1,36 @@
+package com.example.async.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * @author: lichengcan
+ * @date: 2023-09-05 15:59
+ * @description
+ **/
+@RestController
+@RequestMapping("/async")
+public class TestAsync {
+
+    @Autowired
+    TestAsync2 testAsync2;
+
+
+    @GetMapping("/test")
+    public Object test() throws InterruptedException {
+        testAsync2.test1();
+        return "123";
+    }
+
+    @Async
+    public void test1() throws InterruptedException {
+        sleep(6000);
+        System.out.println("1");
+    }
+
+}
